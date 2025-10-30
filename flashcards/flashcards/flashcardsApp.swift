57208b97bc427dde9f2918be4496ab404fb07b6f
avinebel <73241397+avinebel@users.noreply.github.com>
2025-10-30 11:05:58 -0400

    creates base framework like default contentview

---
+++ flashcards/flashcards/flashcardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  flashcardsApp.swift
+//  flashcards
+//
+//  Created by Avi Nebel on 10/28/25.
+//
+
+import SwiftUI
+
+@main
+struct flashcardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
